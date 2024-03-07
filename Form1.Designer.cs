e8dcb0e2278ba07a3f5255223928dd3f1865203b
StephenOosthuizen <StephenDiavolo@gmail.com>
2024-03-07 22:21:25 +0200

    Created an actual front-end that allows variable input. Users can now use the front-end to actually upload and download files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,57 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            btnUploadFile = new Button();
+            txtResult = new RichTextBox();
+            label1 = new Label();
+            tabControlUI = new TabControl();
+            tabPageUpload = new TabPage();
+            btnOpenFileDialog = new Button();
+            txtboxRemoteFolder = new TextBox();
+            txtboxSourceFile = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            tabPageDownload = new TabPage();
+            button1 = new Button();
+            btnDownloadFile = new Button();
+            txtboxLocalDestination = new TextBox();
+            label7 = new Label();
+            txtboxRemoteSource = new TextBox();
+            label6 = new Label();
+            txtboxPassword = new TextBox();
+            txtboxUsername = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            tabControlUI.SuspendLayout();
+            tabPageUpload.SuspendLayout();
+            tabPageDownload.SuspendLayout();
+            SuspendLayout();
             // 
-            // button1
+            // btnUploadFile
             // 
-            this.button1.Location = new System.Drawing.Point(84, 43);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(514, 92);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Upload/Download";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            btnUploadFile.Location = new Point(217, 147);
+            btnUploadFile.Margin = new Padding(4);
+            btnUploadFile.Name = "btnUploadFile";
+            btnUploadFile.Size = new Size(184, 43);
+            btnUploadFile.TabIndex = 0;
+            btnUploadFile.Text = "Upload";
+            btnUploadFile.UseVisualStyleBackColor = true;
+            btnUploadFile.Click += UploadFileToServer_Click;
             // 
-            // richTextBox1
+            // txtResult
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(84, 183);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(518, 221);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
+            txtResult.Location = new Point(12, 453);
+            txtResult.Margin = new Padding(4);
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(611, 169);
+            txtResult.TabIndex = 1;
+            txtResult.Text = "";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 160);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Result";
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 424);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Result";
+            // 
+            // tabControlUI
+            // 
+            tabControlUI.Controls.Add(tabPageUpload);
+            tabControlUI.Controls.Add(tabPageDownload);
+            tabControlUI.Location = new Point(12, 129);
+            tabControlUI.Name = "tabControlUI";
+            tabControlUI.SelectedIndex = 0;
+            tabControlUI.Size = new Size(615, 267);
+            tabControlUI.TabIndex = 3;
+            // 
+            // tabPageUpload
+            // 
+            tabPageUpload.Controls.Add(btnOpenFileDialog);
+            tabPageUpload.Controls.Add(txtboxRemoteFolder);
+            tabPageUpload.Controls.Add(txtboxSourceFile);
+            tabPageUpload.Controls.Add(label3);
+            tabPageUpload.Controls.Add(label2);
+            tabPageUpload.Controls.Add(btnUploadFile);
+            tabPageUpload.Location = new Point(4, 34);
+            tabPageUpload.Name = "tabPageUpload";
+            tabPageUpload.Padding = new Padding(3);
+            tabPageUpload.Size = new Size(607, 229);
+            tabPageUpload.TabIndex = 0;
+            tabPageUpload.Text = "Upload Files";
+            tabPageUpload.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenFileDialog
+            // 
+            btnOpenFileDialog.Location = new Point(451, 20);
+            btnOpenFileDialog.Name = "btnOpenFileDialog";
+            btnOpenFileDialog.Size = new Size(106, 31);
+            btnOpenFileDialog.TabIndex = 7;
+            btnOpenFileDialog.Text = "Open";
+            btnOpenFileDialog.UseVisualStyleBackColor = true;
+            btnOpenFileDialog.Click += OpenSourceFileDialog_Click;
+            // 
+            // txtboxRemoteFolder
+            // 
+            txtboxRemoteFolder.Location = new Point(176, 79);
+            txtboxRemoteFolder.Name = "txtboxRemoteFolder";
+            txtboxRemoteFolder.Size = new Size(269, 31);
+            txtboxRemoteFolder.TabIndex = 6;
+            // 
+            // txtboxSourceFile
+            // 
+            txtboxSourceFile.Location = new Point(176, 20);
+            txtboxSourceFile.Name = "txtboxSourceFile";
+            txtboxSourceFile.Size = new Size(269, 31);
+            txtboxSourceFile.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(29, 82);
+            label3.Name = "label3";
+            label3.Size = new Size(129, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Remote folder:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(141, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Local source file:";
+            // 
+            // tabPageDownload
+            // 
+            tabPageDownload.Controls.Add(button1);
+            tabPageDownload.Controls.Add(btnDownloadFile);
+            tabPageDownload.Controls.Add(txtboxLocalDestination);
+            tabPageDownload.Controls.Add(label7);
+            tabPageDownload.Controls.Add(txtboxRemoteSource);
+            tabPageDownload.Controls.Add(label6);
+            tabPageDownload.Location = new Point(4, 34);
+            tabPageDownload.Name = "tabPageDownload";
+            tabPageDownload.Padding = new Padding(3);
+            tabPageDownload.Size = new Size(607, 229);
+            tabPageDownload.TabIndex = 1;
+            tabPageDownload.Text = "Download Files";
+            tabPageDownload.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(451, 83);
+            button1.Name = "button1";
+            button1.Size = new Size(106, 31);
+            button1.TabIndex = 12;
+            button1.Text = "Open";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += OpenDestinationFolderDialog_Click;
+            // 
+            // btnDownloadFile
+            // 
+            btnDownloadFile.Location = new Point(220, 143);
+            btnDownloadFile.Name = "btnDownloadFile";
+            btnDownloadFile.Size = new Size(159, 47);
+            btnDownloadFile.TabIndex = 4;
+            btnDownloadFile.Text = "Download";
+            btnDownloadFile.UseVisualStyleBackColor = true;
+            btnDownloadFile.Click += btnDownloadFile_Click;
+            // 
+            // txtboxLocalDestination
+            // 
+            txtboxLocalDestination.Location = new Point(176, 83);
+            txtboxLocalDestination.Name = "txtboxLocalDestination";
+            txtboxLocalDestination.Size = new Size(269, 31);
+            txtboxLocalDestination.TabIndex = 3;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(29, 83);
+            label7.Name = "label7";
+            label7.Size = new Size(108, 25);
+            label7.TabIndex = 2;
+            label7.Text = "Local folder:";
+            // 
+            // txtboxRemoteSource
+            // 
+            txtboxRemoteSource.Location = new Point(176, 25);
+            txtboxRemoteSource.Name = "txtboxRemoteSource";
+            txtboxRemoteSource.Size = new Size(269, 31);
+            txtboxRemoteSource.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 25);
+            label6.Name = "label6";
+            label6.Size = new Size(134, 25);
+            label6.TabIndex = 0;
+            label6.Text = "Remote source:";
+            // 
+            // txtboxPassword
+            // 
+            txtboxPassword.Location = new Point(192, 78);
+            txtboxPassword.Name = "txtboxPassword";
+            txtboxPassword.PasswordChar = '*';
+            txtboxPassword.Size = new Size(269, 31);
+            txtboxPassword.TabIndex = 11;
+            // 
+            // txtboxUsername
+            // 
+            txtboxUsername.Location = new Point(192, 24);
+            txtboxUsername.Name = "txtboxUsername";
+            txtboxUsername.Size = new Size(269, 31);
+            txtboxUsername.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(45, 78);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 25);
+            label5.TabIndex = 9;
+            label5.Text = "Password:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(45, 24);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 25);
+            label4.TabIndex = 8;
+            label4.Text = "Username:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(689, 456);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Stephen Simple SFTP client";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(641, 650);
+            Controls.Add(txtboxPassword);
+            Controls.Add(txtboxUsername);
+            Controls.Add(tabControlUI);
+            Controls.Add(label5);
+            Controls.Add(txtResult);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Stephen Simple SFTP client";
+            tabControlUI.ResumeLayout(false);
+            tabPageUpload.ResumeLayout(false);
+            tabPageUpload.PerformLayout();
+            tabPageDownload.ResumeLayout(false);
+            tabPageDownload.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private RichTextBox richTextBox1;
+        private Button btnUploadFile;
+        private RichTextBox txtResult;
         private Label label1;
+        private TabControl tabControlUI;
+        private TabPage tabPageUpload;
+        private TabPage tabPageDownload;
+        private TextBox txtboxRemoteFolder;
+        private TextBox txtboxSourceFile;
+        private Label label3;
+        private Label label2;
+        private Button btnOpenFileDialog;
+        private TextBox txtboxPassword;
+        private TextBox txtboxUsername;
+        private Label label5;
+        private Label label4;
+        private TextBox txtboxLocalDestination;
+        private Label label7;
+        private TextBox txtboxRemoteSource;
+        private Label label6;
+        private Button btnDownloadFile;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.IO;
 using static System.Net.WebRequestMethods;
 using System.Xml.Linq;
+using System.Windows.Forms;
 
 //file scoped namespace
 namespace C_Sharp_SFTP_Client;
@@ -21,20 +22,40 @@ public partial class Form1 : Form
     //download vars
     private string _remoteDirectory = "";
     private string _localDirectory = @"c:\downloads\";
-    private string _finalDir = "";
+    private string _finalDir = ""; //this will create a copy of _remoteDir each time we download a file from the remote folder, leave as-is 
 
     public Form1()
     {
         InitializeComponent();
     }
 
-    private void button1_Click(object sender, EventArgs e)
+    private bool ValidateDetails()
     {
-        //just have a single method here to do both uploading/downloading, but in reality, we'd split this out into separate methods for unit testing and control purposes
-        SendAndReceiveDataFromSFTPServer();
+        if (!string.IsNullOrWhiteSpace(txtboxSourceFile.Text) && !string.IsNullOrWhiteSpace(txtboxRemoteFolder.Text) && !string.IsNullOrWhiteSpace(txtboxUsername.Text) && !string.IsNullOrWhiteSpace(txtboxPassword.Text))
+        {
+            //load the user's paths and creds
+            _pathOfFile = txtboxSourceFile.Text;
+            _sftpDataInPath = txtboxRemoteFolder.Text;
+            _myUser = txtboxUsername.Text;
+            _myPassword = txtboxPassword.Text;
+
+            return true;
+        }
+        return false;
     }
 
-    private void SendAndReceiveDataFromSFTPServer()
+    private void UploadFileToServer_Click(object sender, EventArgs e)
+    {
+        if (ValidateDetails())
+        {
+            //only attempt to send to remote server once the folders and user credentials are loaded (SFTP requires creds)
+            SendDataToSFTPServer();
+        }
+        else
+            txtResult.Text = "Missing details. Unable to perform action";
+    }
+
+    private void SendDataToSFTPServer()
     {
         string name = Path.GetFileName(_pathOfFile);
         try
@@ -52,12 +73,33 @@ public partial class Form1 : Form
                     }
                     #endregion upload files to remote server
 
-                    richTextBox1.Text = "Uploaded Successfully. Name: " + name.ToString();
+                    txtResult.Text = "Uploaded Successfully. Name: " + name.ToString();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            txtResult.Text = ex.Message;
+        }
+    }
 
+
+    private void DownloadFilesFromSFTPServer()
+    {
+        string name = Path.GetFileName(_pathOfFile);
+        try
+        {
+            //this SftpClient is coming from our Renci dependency
+            using (SftpClient sftp = new SftpClient(_myServer, _myPort, _myUser, _myPassword))
+            {
+                sftp.Connect();
+                if (sftp.IsConnected)
+                {
                     #region download the files from the remote server
 
-                    var files = sftp.ListDirectory("");
+                    var files = sftp.ListDirectory(txtboxRemoteSource.Text);
 
+                    //download all files in the directory
                     foreach (var file in files)
                     {
                         string remoteFileName = file.Name;
@@ -65,7 +107,7 @@ public partial class Form1 : Form
                         //we need to check that we only download today's files
                         if ((!file.Name.StartsWith(".")) && ((file.LastWriteTime.Date == DateTime.Today)))
                         {
-                            _finalDir = _localDirectory;
+                            _finalDir = _localDirectory; //dont want to overwrite my _localDir var, so using _finalDir to copy value each time
 
                             if (System.IO.File.Exists(_finalDir + file.Name))
                             {
@@ -76,7 +118,7 @@ public partial class Form1 : Form
                                 using (Stream file1 = System.IO.File.OpenWrite(_finalDir + remoteFileName))
                                 {
                                     sftp.DownloadFile(_remoteDirectory + remoteFileName, file1);
-                                    richTextBox1.Text += "\r\nDownloaded successfully. Name: " + remoteFileName.ToString();
+                                    txtResult.Text += "\r\nDownloaded successfully. Name: " + remoteFileName.ToString();
                                 }
                             }
                         }
@@ -85,9 +127,57 @@ public partial class Form1 : Form
                 }
             }
         }
-        catch (Exception ex)
+        catch (Exception ex) 
         {
-            richTextBox1.Text = ex.Message;
+            txtResult.Text = ex.Message;
         }
+    }
+    private void OpenSourceFileDialog_Click(object sender, EventArgs e)
+    {
+        //open a new file dialog window - lets our user choose files
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            try
+            {
+                txtboxSourceFile.Text = openFileDialog.FileName;
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = $"Error with source file: {ex.Message}";
+            }
+        }
+
+    }
+
+    private void OpenDestinationFolderDialog_Click(object sender, EventArgs e)
+    {
+        //open a new file dialog window - lets our user choose files
+        FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
+
+        if (openFolderDialog.ShowDialog() == DialogResult.OK)
+        {
+            try
+            {
+                txtboxLocalDestination.Text = openFolderDialog.SelectedPath;
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = $"Error with source file: {ex.Message}";
+            }
+        }
+    }
+
+    private void btnDownloadFile_Click(object sender, EventArgs e)
+    {
+        if (ValidateDetails() && !string.IsNullOrWhiteSpace(txtboxLocalDestination.Text))
+        {
+            _localDirectory = txtboxLocalDestination.Text;
+            DownloadFilesFromSFTPServer();
+        }
+        else
+            txtResult.Text = "Missing details. Unable to perform action";
+        
     }
 }
